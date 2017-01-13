63285d90020445f6383b116c03d25c2749d8e180
kang yoosam <yooska14@gmail.com>
2017-01-13 21:58:28 +0900

    ray

--- Assets/Scripts/RayManager.cs
+++ Assets/Scripts/RayManager.cs
@@ -10,20 +10,23 @@ public class RayManager : MonoBehaviour {
 	public int endPositionX = -4;
 	float gaugeTime;
 	Vector3 firstButtonGaugePosition;
+	LineRenderer linerenderer;
 
 	void Start () {
 		firstButtonGaugePosition = buttonGauge.transform.localPosition;
+		linerenderer = GetComponent<LineRenderer> ();
 	}
 
 	void Update () {
 		Ray ray = new Ray (diveCamera.transform.position, diveCamera.transform.forward);
 		RaycastHit hit;
 
+		linerenderer.SetPosition (0, transform.position);
+
 		if (Physics.Raycast (ray, out hit)) {
 			Debug.DrawLine (ray.origin, hit.point, Color.black);
 
 			if (hit.transform.name == "Main" || hit.transform.name == "Kuppa") {
-				print (hit.transform.name);
 				gaugeTime += Time.deltaTime * 0.01f;
 				buttonGauge.rectTransform.localPosition = Vector3.Lerp (buttonGauge.rectTransform.localPosition, new Vector3 (1400, 0, 1), gaugeTime);
 			} else {
@@ -34,5 +37,6 @@ public class RayManager : MonoBehaviour {
 				SceneManager.LoadScene (hit.transform.name);
 			}
 		}
+		linerenderer.SetPosition (1, ray.origin + ray.direction * 100f);
 	}
 }
